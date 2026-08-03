8974072c7b22ed2d739033da007007ef402b191f
Ronny Gruenewald <rg@lothar-medtec.de>
2026-08-03 16:51:25 +0200

    test(composer): harden clipboard fixture sanitization

--- tests/composer-send-lock.test.js
+++ tests/composer-send-lock.test.js
@@ -124,6 +124,7 @@ global.DOMParser = class DOMParser {
 							.replace(/<\/(p|div|li|h[1-6])>/gi, "\n")
 							.replace(/<br\s*\/?>/gi, "\n")
 							.replace(/<[^>]*>/g, "")
+							.replace(/[<>]/g, "")
 							.trim(),
 					},
 				],
